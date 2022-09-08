480a913b6b483fee9ee080f6e062fff22e068871
Greg Boyer <greg@gregoryboyer.com>
2022-09-08 00:36:56 -0700

    Added on/off button

--- code.cpp
+++ code.cpp
@@ -3,12 +3,20 @@ const int buttonPin = 2;     // the number of the pushbutton pin
 const int ledPin1 =  6; 
 const int ledPin2 =  5; 
 
+
 int activeLed = ledPin1;
 
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-
+//int buttonState = 0;         // variable for reading the pushbutton status
+  unsigned long pressStartTime;
+  unsigned long pressStopTime;
+  unsigned long stateStartTime;
+  unsigned long buttonHoldTime = 0;
+  int lastButtonState = 0;
+  int currentButtonState = 0;
+  bool onOffState = 0;
+  
 void setup(){
   pinMode(buttonPin,INPUT); // what does this do?
   pinMode(ledPin2,OUTPUT);
@@ -16,33 +24,61 @@ void setup(){
   Serial.begin(9600);
 } 
 
+// redifined functionality:
+// while holding button, time is tracked, if time reaches 2 seconds
+// all clocks start or stop
 
 void loop() {
   // read the state of the pushbutton value:
+
+  currentButtonState = digitalRead(buttonPin);
   
-  buttonState = digitalRead(buttonPin);
-  if (buttonState==HIGH){
-    switch (activeLed){
-      case ledPin1:
-        digitalWrite(activeLed, LOW);
-        activeLed = ledPin2;
-        digitalWrite(activeLed, HIGH);
-      	Serial.println("change to pin2");
-        break;
-      case ledPin2:
-        digitalWrite(activeLed, LOW);
-        activeLed = ledPin1;
-        digitalWrite(activeLed, HIGH); 
-      	Serial.println("change to pin1");
-        break;
-    Serial.println("test");
-    Serial.println(activeLed);
+  // State detection for button
+  if (currentButtonState != lastButtonState) {
+    stateStartTime = millis();
+    lastButtonState = currentButtonState;
+    Serial.println(stateStartTime);
+    
+    // if currentButtonState == LOW
+  }
+  
+  // keep track of button pressed time
+  if (currentButtonState == HIGH){
+    buttonHoldTime = millis() - stateStartTime;
+    
+    Serial.println("Press hold time:");
+    Serial.println(buttonHoldTime);
+    
+    // if button held 3s, blink leds, turn state to "off"
+    
+    // todo: need more state detection in a separate function. 
+    // when turned on or off, what happens to the light or sound
+    if (buttonHoldTime > 3000) {
+      onOffState = onOff(onOffState);
+      Serial.println(onOffState);
+      }
+      
     }
-  delay (100);         // debounce
-  }
+    // need an on-off state
+  } 
+//     else {
+//    buttonHoldTime=0;
+//  }
+      
+      // if button has been held 3 seconds, start or stop both clocks
+
+ 
+bool onOff(bool onOffState) {
+
+  for ( int x = 0; x < 3; x++ ) {
+    digitalWrite(activeLed, LOW);
+    delay(200);
+    digitalWrite(activeLed, HIGH);
+    delay(500);
+    
+  onOffState = !onOffState;
   
-  
+  return onOffState;
   }
-
-
-
+}
+  
